2b5e593986349ef47e37a56adb45f94d84c89ff5
radhika7171 <aairamahajan@gmail.com>
2021-07-15 22:26:53 -0400

    customizing amin page

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -7,9 +8,12 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
 
-    def __str__(self):
-        return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
